9251677fb83227a829c19edfcfe5de965debe143
balintsipos0306 <103211327+balintsipos0306@users.noreply.github.com>
2025-03-29 22:38:56 +0100

    username alignment in webshop with opened item

--- resources/views/webshopItem.blade.php
+++ resources/views/webshopItem.blade.php
@@ -204,11 +204,11 @@
     </nav>
     <div class="container">
         <ul id="iconList" class="nav">
-        <li class="nav-item">
-       @if (!empty(Auth()->user()->name))
+        @if (!empty(Auth()->user()->name))
+            <li class="nav-item" id="nameHolder">
               <p id="username">{{Auth()->user()->name}}</p>
-            @endif
-       </li>
+            </li>
+          @endif
         <li class="nav-item">
             <a data-bs-toggle="modal" data-bs-target="#login"><img id="icon" src="../../webp/user.png" alt=""></a>
         </li>
