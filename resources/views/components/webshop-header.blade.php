cc8564ba2b71be4b5d018e6898f4870c9f045de2
balintsipos0306 <103211327+balintsipos0306@users.noreply.github.com>
2025-09-24 20:05:10 +0200

    webshop UI cleanup - header to separate component

---
+++ resources/views/components/webshop-header.blade.php
@@ -0,0 +1,61 @@
+<link rel="stylesheet" href="{{ asset('css/baseHeader.css') }}">
+<link rel="stylesheet" href="{{ asset('css/webshopHeader.css') }}">
+
+<div>
+     <header id="myheader">
+    <div class="hamburger">
+      <span class="bar"></span>
+      <span class="bar"></span>
+      <span class="bar"></span>
+    </div>
+    <div class= "logoholder">
+      <img id = "logo" src="../../webp/tinywow_Logó.webp" alt="">
+    </div>
+
+    <nav id = "navv">
+      <ul class="nav justify-content-end" id="menu">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="/">Kezdőlap</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/gallery/nature">Galéria</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/contact">Elérhetőségek</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/blog">Blog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/shop">Webshop</a>
+        </li>
+      </ul>
+    </nav>
+    <div class="container">
+      <ul id="iconList" class="nav">
+          @if (!empty(Auth()->user()->name))
+            <li class="nav-item" id="nameHolder">
+              <p id="username">{{Auth()->user()->name}}</p>
+            </li>
+          @endif
+        <li class="nav-item">
+          <a data-bs-toggle="modal" data-bs-target="#login"><img id="icon" src="../../webp/user.png" alt=""></a>
+        </li>
+        <li class="nav-item">
+          @if (empty(Auth()->user()->name))
+            <a data-bs-toggle="offcanvas" href="#cart" role="button" aria-controls="offcanvasExample"><img id="icon" src="../../webp/shopping-cart.png" alt=""></a>
+          @else
+            @php
+              $cartitems = DB::table('cart')->where('userID', Auth()->user()->id)->first();
+            @endphp
+            @if (empty($cartitems))
+              <a data-bs-toggle="offcanvas" href="#cart" role="button" aria-controls="offcanvasExample"><img id="icon" src="../../webp/shopping-cart.png" alt=""></a>
+            @else
+              <a data-bs-toggle="offcanvas" href="#cart" role="button" aria-controls="offcanvasExample"><img id="icon" src="../../webp/shopping-cart-item.png" alt=""></a>   
+            @endif
+          @endif
+        </li>
+      </ul>
+    </div>
+  </header>
+</div>
