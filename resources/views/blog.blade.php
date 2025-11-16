e537c54a8b583ab8f116b57c04df64e574ffdc83
balintsipos0306 <103211327+balintsipos0306@users.noreply.github.com>
2025-11-16 18:20:04 +0100

    blog cleanup - link fix

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -16,8 +16,6 @@
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
   <script src="js/scroll.js"></script>
-  <script src="js/hamburger.js"></script>
-  <script src="js/adminLogin.js"></script>
 
   <x-system-feedback/>
   <x-base-header/>
@@ -27,28 +25,7 @@
       $blogs = DB::table('blogs')->where('isPublished', "publikált")->get();
     @endphp
 
-    <div class="container">
-      <div id="sub" class="card text-bg-success mb-3">
-        <div class="card-header">Feliratkozás</div>
-        <div class="card-body">
-          <h5 class="card-title">Iratkozz fel a körlevélre, hogy emailben értesülj az új bejegyzésekről</h5>
-          <form action="/sub" method="POST">
-            @csrf
-            <div class="input-group" id="inputs">
-              <div class="g col-4">
-                <label for="name" class="form-label">Keresztnév</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="name" name="name">
-              </div>
-              <div class="g col-6" id="email">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="name" name="email">
-              </div>
-            </div>
-            <button type="submit" class="btn btn-light">Feliratkozás</button>
-          </form>
-        </div>
-      </div>
-    </div>
+    <x-subscribe-card/>
 
     <div class="row d-flex flex-nowrap" id="felsorolas">
       @foreach ($blogs as $blog)
